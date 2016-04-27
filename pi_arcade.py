578fa0ad2fc882106dc022016f4472f0b3b59b14
StephenHenning <shenning@student.waubonsee.edu>
2016-04-27 14:49:41 -0500

    projectile sprite group
    I commented out the projectile sprite group line
    I am not sure where the pos and velocity vectors are derived

--- pi_arcade.py
+++ pi_arcade.py
@@ -14,7 +14,8 @@ from pygame.math import Vector2
 from pygame.sprite import Group
 from pygame.sprite import GroupSingle
 
-from spritesheet import  SpriteSheet
+from projectile import Projectile
+from spritesheet import SpriteSheet
 from player import Player
 
 # Initialize pygame
@@ -32,17 +33,20 @@ clock = Clock()
 running = True
 
 # Sprites & Sprite Groups
-player = Player(SpriteSheet("resources/img/player.png").images_at(util.gen_sprite_list(7, 7, 128, 192, 0), [255, 0, 255]), 64, Vector2(128, pi_globals.screenSize[1] / 2.0))
+player = Player(SpriteSheet("resources/img/player.png").images_at(util.gen_sprite_list(7, 7, 128, 192, 0), [255, 0, 255]), 64,Vector2(128, pi_globals.screenSize[1] / 2.0))
 player.animController.enabled = True
+#projectile = Projectile(SpriteSheet("resources/img/paper_animation.png").image_at(util.gen_sprite_list(26, 10, 50, 50, 0), [255, 255, 255]), 25, player.position, Vector2(25, 25), 0.0)
+
 
 def endTrowTrigger(targetPlayer):
-	targetPlayer.animController.setAnimStrip(0, 1, 18.0)
+    targetPlayer.animController.setAnimStrip(0, 1, 18.0)
+
 
 player.animController.setAnimStrip(0, 1, 18.0)
 player.animController.setTrigger(6, endTrowTrigger)
 
 playerGroup = GroupSingle(player)
-projectileGroup = Group()
+projectileGroup = Group(projectile)
 
 # Joystick initialization
 # TODO Move joystick stuff to a input management file
@@ -51,47 +55,51 @@ hasJoystick = False
 joystick = None
 
 if pygame.joystick.get_count() > 0:
-	joystick = pygame.joystick.Joystick(0)
-	joystick.init()
-	hasJoystick = joystick.get_numaxes() >= 2
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    hasJoystick = joystick.get_numaxes() >= 2
 
 print("Has Joystick: " + str(hasJoystick))
 
 # -------- Main Program Loop -----------
 while running:
 
-	# TODO Move input event stuff to input management file
-	# Do we even need to use this? All input will be coming from joystick stuff
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				running = False
-			if event.key == pygame.K_SPACE:
-				player.animController.setAnimStrip(1, 6, 18.0)
+    # TODO Move input event stuff to input management file
+    # Do we even need to use this? All input will be coming from joystick stuff
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            if event.key == pygame.K_SPACE:
+                player.animController.setAnimStrip(1, 6, 18.0)
+        projectile = Projectile()
+        # I dont think we need x position, player only moves along y axis
+        projectile.rect.x = player.rect.x
+        projectile.rect.y = player.rect.y
 
-	screenSurface.fill(pi_globals.BLACK)
+    screenSurface.fill(pi_globals.BLACK)
 
-	position = Vector2(0, 0)
-	pressedKeys = pygame.key.get_pressed()
+    position = Vector2(0, 0)
+    pressedKeys = pygame.key.get_pressed()
 
-	if pressedKeys[pygame.K_w] or pressedKeys[pygame.K_UP]:
-		position.y -= 1
+    if pressedKeys[pygame.K_w] or pressedKeys[pygame.K_UP]:
+        position.y -= 1
 
-	if pressedKeys[pygame.K_s] or pressedKeys[pygame.K_DOWN]:
-		position.y += 1
+    if pressedKeys[pygame.K_s] or pressedKeys[pygame.K_DOWN]:
+        position.y += 1
 
-	# Hard Coded Speed
-	player.position += position * 10;
+    # Hard Coded Speed
+    player.position += position * 10;
 
-	playerGroup.update()
+    playerGroup.update()
 
-	playerGroup.draw(screenSurface)
+    playerGroup.draw(screenSurface)
 
-	# Update screen with what has been drawn
-	display.flip()
+    # Update screen with what has been drawn
+    display.flip()
 
-	# Game loop clock tick and delta time calculation
-	pi_globals.deltaTime = clock.tick_busy_loop(pi_globals.FRAMERATE) / 1000.0
+    # Game loop clock tick and delta time calculation
+    pi_globals.deltaTime = clock.tick_busy_loop(pi_globals.FRAMERATE) / 1000.0
 
 # Close the window and quit.
 pygame.quit()
@@ -157,171 +165,171 @@ pygame.quit()
 # deltaTime = 1 / framerate
 # joyTolerance = 0.2
 
-	# --- Main event loop
-	# for event in pygame.event.get():
-	# 	if event.type == pygame.QUIT:
-	# 		running = False
-	# 	if event.type == pygame.KEYDOWN:
-	# 		if event.key == pygame.K_r:
-	# 			player.sprite.set_anim_framerate(-player.sprite.get_anim_framerate())
-	# 		if event.key == pygame.K_PAGEUP:
-	# 			player.sprite.set_anim_framerate(player.sprite.get_anim_framerate() + 5)
-	# 		if event.key == pygame.K_PAGEDOWN:
-	# 			player.sprite.set_anim_framerate(player.sprite.get_anim_framerate() - 5)
-	#
-	# # --- Game logic should go here
-	#
-	# # Input
-	# movement = [0.0, 0.0]
-	#
-	# if hasJoystick:
-	# 	# Use joystick if one exists
-	#
-	# 	# D-Pad (digital joystick)
-	# 	# hat = joystick.get_hat(0)
-	# 	# movement[0] = hat[0]
-	# 	# movement[1] = -hat[1]
-	#
-	# 	# Actual Joystick (analog joystick)
-	# 	movement[0] = joystick.get_axis(0)
-	# 	movement[1] = joystick.get_axis(1)
-	#
-	# 	# Tolerance check to correct hardware imprecision
-	# 	if abs(movement[0]) < joyTolerance:
-	# 		movement[0] = 0
-	#
-	# 	if abs(movement[1]) < joyTolerance:
-	# 		movement[1] = 0
-	#
-	# else:
-	# 	# Use keyboard if joystick does not exist
-	# 	pressedKeys = pygame.key.get_pressed()
-	#
-	# 	# Input logic for keys designed to zero out movement when
-	# 	# both keys on axis are pressed (up/down or left/right)
-	# 	if pressedKeys[pygame.K_a] or pressedKeys[pygame.K_LEFT]:
-	# 		movement[0] -= 1
-	#
-	# 	if pressedKeys[pygame.K_d] or pressedKeys[pygame.K_RIGHT]:
-	# 		movement[0] += 1
-	#
-	# 	if pressedKeys[pygame.K_w] or pressedKeys[pygame.K_UP]:
-	# 		movement[1] -= 1
-	#
-	# 	if pressedKeys[pygame.K_s] or pressedKeys[pygame.K_DOWN]:
-	# 		movement[1] += 1
-	#
-	# # Normalize movement
-	# sqMagnitude = (movement[0] * movement[0]) + (movement[1] * movement[1])
-	# if sqMagnitude > 1:
-	# 	magnitude = math.sqrt(sqMagnitude)
-	# 	movement[0] /= magnitude
-	# 	movement[1] /= magnitude
-	#
-	# # Apply movement modified by speed and delta time to position
-	# position[0] += movement[0] * speed * deltaTime
-	# position[1] += movement[1] * speed * deltaTime
-	#
-	# # Clamp position to screen
-	# if position[0] < 0 + circleRadius:
-	# 	position[0] = 0 + circleRadius
-	#
-	# if position[0] > size[0] - circleRadius:
-	# 	position[0] = size[0] - circleRadius
-	#
-	# if position[1] < 0 + circleRadius:
-	# 	position[1] = 0 + circleRadius
-	#
-	# if position[1] > size[1] - circleRadius:
-	# 	position[1] = size[1] - circleRadius
-	#
-	# player.update(deltaTime, position)
-	#
-	# # Debug position and delta time
-	# # print(str(position[0]) + " : " + str(position[1]) + " : " + str(deltaTime));
-	#
-	# # --- Screen-clearing code goes here
-	#
-	# # Here, we clear the screen to white. Don't put other drawing commands
-	# # above this, or they will be erased with this command.
-	#
-	# # If you want a background image, replace this clear with blit'ing the
-	# # background image.
-	# # screen.fill(WHITE)
-	# screen.blit(background, (0, 0))
-	#
-	# # --- Drawing code should go here
-	#
-	# # Draw a circle at position
-	# # pygame.draw.circle(screen, BLUE, [int(position[0]), int(position[1])], circleRadius)
-	# player.draw(screen)
-	#
-	# # --- Go ahead and update the screen with what we've drawn.
-	# pygame.display.flip()
-	#
-	# # --- Limit to 60 frames per second
-	# deltaTime = clock.tick_busy_loop(framerate) / 1000.0
+# --- Main event loop
+# for event in pygame.event.get():
+# 	if event.type == pygame.QUIT:
+# 		running = False
+# 	if event.type == pygame.KEYDOWN:
+# 		if event.key == pygame.K_r:
+# 			player.sprite.set_anim_framerate(-player.sprite.get_anim_framerate())
+# 		if event.key == pygame.K_PAGEUP:
+# 			player.sprite.set_anim_framerate(player.sprite.get_anim_framerate() + 5)
+# 		if event.key == pygame.K_PAGEDOWN:
+# 			player.sprite.set_anim_framerate(player.sprite.get_anim_framerate() - 5)
+#
+# # --- Game logic should go here
+#
+# # Input
+# movement = [0.0, 0.0]
+#
+# if hasJoystick:
+# 	# Use joystick if one exists
+#
+# 	# D-Pad (digital joystick)
+# 	# hat = joystick.get_hat(0)
+# 	# movement[0] = hat[0]
+# 	# movement[1] = -hat[1]
+#
+# 	# Actual Joystick (analog joystick)
+# 	movement[0] = joystick.get_axis(0)
+# 	movement[1] = joystick.get_axis(1)
+#
+# 	# Tolerance check to correct hardware imprecision
+# 	if abs(movement[0]) < joyTolerance:
+# 		movement[0] = 0
+#
+# 	if abs(movement[1]) < joyTolerance:
+# 		movement[1] = 0
+#
+# else:
+# 	# Use keyboard if joystick does not exist
+# 	pressedKeys = pygame.key.get_pressed()
+#
+# 	# Input logic for keys designed to zero out movement when
+# 	# both keys on axis are pressed (up/down or left/right)
+# 	if pressedKeys[pygame.K_a] or pressedKeys[pygame.K_LEFT]:
+# 		movement[0] -= 1
+#
+# 	if pressedKeys[pygame.K_d] or pressedKeys[pygame.K_RIGHT]:
+# 		movement[0] += 1
+#
+# 	if pressedKeys[pygame.K_w] or pressedKeys[pygame.K_UP]:
+# 		movement[1] -= 1
+#
+# 	if pressedKeys[pygame.K_s] or pressedKeys[pygame.K_DOWN]:
+# 		movement[1] += 1
+#
+# # Normalize movement
+# sqMagnitude = (movement[0] * movement[0]) + (movement[1] * movement[1])
+# if sqMagnitude > 1:
+# 	magnitude = math.sqrt(sqMagnitude)
+# 	movement[0] /= magnitude
+# 	movement[1] /= magnitude
+#
+# # Apply movement modified by speed and delta time to position
+# position[0] += movement[0] * speed * deltaTime
+# position[1] += movement[1] * speed * deltaTime
+#
+# # Clamp position to screen
+# if position[0] < 0 + circleRadius:
+# 	position[0] = 0 + circleRadius
+#
+# if position[0] > size[0] - circleRadius:
+# 	position[0] = size[0] - circleRadius
+#
+# if position[1] < 0 + circleRadius:
+# 	position[1] = 0 + circleRadius
+#
+# if position[1] > size[1] - circleRadius:
+# 	position[1] = size[1] - circleRadius
+#
+# player.update(deltaTime, position)
+#
+# # Debug position and delta time
+# # print(str(position[0]) + " : " + str(position[1]) + " : " + str(deltaTime));
+#
+# # --- Screen-clearing code goes here
+#
+# # Here, we clear the screen to white. Don't put other drawing commands
+# # above this, or they will be erased with this command.
+#
+# # If you want a background image, replace this clear with blit'ing the
+# # background image.
+# # screen.fill(WHITE)
+# screen.blit(background, (0, 0))
+#
+# # --- Drawing code should go here
+#
+# # Draw a circle at position
+# # pygame.draw.circle(screen, BLUE, [int(position[0]), int(position[1])], circleRadius)
+# player.draw(screen)
+#
+# # --- Go ahead and update the screen with what we've drawn.
+# pygame.display.flip()
+#
+# # --- Limit to 60 frames per second
+# deltaTime = clock.tick_busy_loop(framerate) / 1000.0
 
 ######## HARD-CODED SPRITE LOCATION LISTS ##########
 
 
-		# Spinning ball
-		# self.sprites = self.spriteSheet.images_at(
-		#	 [
-		#		 (0, 0, 64, 64),
-		#		 (64, 0, 64, 64),
-		#		 (128, 0, 64, 64),
-		#		 (192, 0, 64, 64),
-		#		 (256, 0, 64, 64),
-		#		 (320, 0, 64, 64),
-		#		 (384, 0, 64, 64),
-		#		 (448, 0, 64, 64),
-		#		 (0, 64, 64, 64),
-		#		 (64, 64, 64, 64),
-		#		 (128, 64, 64, 64),
-		#		 (192, 64, 64, 64),
-		#		 (256, 64, 64, 64),
-		#		 (320, 64, 64, 64),
-		#		 (384, 64, 64, 64),
-		#		 (448, 64, 64, 64),
-		#		 (0, 128, 64, 64),
-		#		 (64, 128, 64, 64),
-		#		 (128, 128, 64, 64),
-		#		 (192, 128, 64, 64),
-		#		 (256, 128, 64, 64),
-		#		 (320, 128, 64, 64),
-		#		 (384, 128, 64, 64),
-		#		 (448, 128, 64, 64),
-		#		 (0, 192, 64, 64),
-		#		 (64, 192, 64, 64),
-		#		 (128, 192, 64, 64),
-		#		 (192, 192, 64, 64),
-		#		 (256, 192, 64, 64),
-		#		 (320, 192, 64, 64),
-		#		 (384, 192, 64, 64),
-		#		 (448, 192, 64, 64),
-		#	 ], [255, 0, 255]  # Color Key for alpha
-		# )
-		# self.spriteCount = 32
+# Spinning ball
+# self.sprites = self.spriteSheet.images_at(
+#	 [
+#		 (0, 0, 64, 64),
+#		 (64, 0, 64, 64),
+#		 (128, 0, 64, 64),
+#		 (192, 0, 64, 64),
+#		 (256, 0, 64, 64),
+#		 (320, 0, 64, 64),
+#		 (384, 0, 64, 64),
+#		 (448, 0, 64, 64),
+#		 (0, 64, 64, 64),
+#		 (64, 64, 64, 64),
+#		 (128, 64, 64, 64),
+#		 (192, 64, 64, 64),
+#		 (256, 64, 64, 64),
+#		 (320, 64, 64, 64),
+#		 (384, 64, 64, 64),
+#		 (448, 64, 64, 64),
+#		 (0, 128, 64, 64),
+#		 (64, 128, 64, 64),
+#		 (128, 128, 64, 64),
+#		 (192, 128, 64, 64),
+#		 (256, 128, 64, 64),
+#		 (320, 128, 64, 64),
+#		 (384, 128, 64, 64),
+#		 (448, 128, 64, 64),
+#		 (0, 192, 64, 64),
+#		 (64, 192, 64, 64),
+#		 (128, 192, 64, 64),
+#		 (192, 192, 64, 64),
+#		 (256, 192, 64, 64),
+#		 (320, 192, 64, 64),
+#		 (384, 192, 64, 64),
+#		 (448, 192, 64, 64),
+#	 ], [255, 0, 255]  # Color Key for alpha
+# )
+# self.spriteCount = 32
 
-		# Spinning paper
-		# self.sprites = self.spriteSheet.images_at(
-		#	 [
-		#		 (0, 0, 64, 64),
-		#		 (64, 0, 64, 64),
-		#		 (128, 0, 64, 64),
-		#		 (192, 0, 64, 64),
-		#		 (256, 0, 64, 64),
-		#		 (320, 0, 64, 64),
-		#		 (384, 0, 64, 64),
-		#		 (448, 0, 64, 64),
-		#		 (512, 0, 64, 64),
-		#		 (576, 0, 64, 64),
-		#	 ], [255, 0, 255]  # Color Key for alpha
-		# )
-		# self.spriteCount = 10
+# Spinning paper
+# self.sprites = self.spriteSheet.images_at(
+#	 [
+#		 (0, 0, 64, 64),
+#		 (64, 0, 64, 64),
+#		 (128, 0, 64, 64),
+#		 (192, 0, 64, 64),
+#		 (256, 0, 64, 64),
+#		 (320, 0, 64, 64),
+#		 (384, 0, 64, 64),
+#		 (448, 0, 64, 64),
+#		 (512, 0, 64, 64),
+#		 (576, 0, 64, 64),
+#	 ], [255, 0, 255]  # Color Key for alpha
+# )
+# self.spriteCount = 10
 
-		# Rollimg Paper
+# Rollimg Paper
 '''
 self.sprites = self.spriteSheet.images_at(
 	[
@@ -344,4 +352,4 @@ self.sprites = self.spriteSheet.images_at(
 	], [255, 0, 255]  # Color Key for alpha
 )
 self.spriteCount = 16
-'''	
+'''
--- player.py
+++ player.py
@@ -3,41 +3,38 @@ from pygame.sprite import *
 
 from animation import AnimationController
 
+
 class Player(Sprite):
+    __animController = None
 
-	__animController = None
+    position = Vector2(0, 0)
 
-	position = Vector2(0, 0)
+    def __init__(self, spriteSheet, collisionRadius, position):
+        Sprite.__init__(self)
+        self.image = spriteSheet[0]
+        self.rect = self.image.get_rect()
+        self.radius = collisionRadius
+        self.position = position
+        self.__animController = AnimationController(self, spriteSheet)
 
-	def __init__(self, spriteSheet, collisionRadius, position):
-		Sprite.__init__(self)
-		self.image = spriteSheet[0]
-		self.rect = self.image.get_rect()
-		self.radius = collisionRadius
-		self.position = position
-		self.__animController = AnimationController(self, spriteSheet)
+    def getAnimController(self):
+        return self.__animController
 
-	def getAnimController(self):
-		return self.__animController
+    animController = property(getAnimController)
 
-	animController = property(getAnimController)
+    def getCollisionRadius(self):
+        return self.rect.radius
 
-	def getCollisionRadius(self):
-		return self.rect.radius
+    collisionRadius = property(getCollisionRadius)
 
-	collisionRadius = property(getCollisionRadius)
+    # Args[0] is must be delta time (amount in seconds between frames)
+    def update(self):
+        # Animation
+        self.animController.update()
 
-	# Args[0] is must be delta time (amount in seconds between frames)
-	def update(self):
+        # Update rect center with position
+        self.rect.center = self.position
 
-		# Animation
-		self.animController.update()
-
-		# Update rect center with position
-		self.rect.center = self.position
-
-	def Shoot(self):
-        #Change center to tiop half
-		bullet_origin = self.rect.center
-
-
+    def Shoot(self):
+        # Change center to top half
+        bullet_origin = self.rect.center + 5
